ea8a6f0bf1ea6f99cb3248f0e6354c935458f40c
valcomp <valerie.aleida.lopez@gmail.com>
2022-08-13 00:13:37 -0500

    added offline persistence with indexedDb

---
+++ public/js/idb.js
@@ -0,0 +1,89 @@
+//variable to hold db connection
+let db;
+
+//establish a connection to idb and set it to version 1
+const request = indexedDB.open('track_budget',1);
+
+//this event will emit if the database version changes
+request.onupgradeneeded = function(event){
+    //save a reference to the database
+    const db = event.target.result;
+    //create an object store(table) called 'new_budget', set it to have an auto
+    //incrementing primary key of sorts
+    db.createObjectStore('new_budget',{autoIncrement:true});
+};
+
+//upon a successful
+request.onsuccess = function(event){
+    //when db is successful with object store or establishes connection
+    //save reference to db in global variable
+    db = event.target.result
+
+    //check if app is online, if yes run the function to send local db to api
+    if(navigator.onLine){
+        uploadData();
+    }
+};
+
+request.onerror = function(event){
+    //log error here
+    console.log(event.target.errorCode);
+};
+
+
+//this function will be executed if we attempt to add another cost and theres no internet connection
+function saveRecord(record) {
+    //open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    //access the object store for 'new_budget'
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    // add record to your store with add method.
+    budgetObjectStore.add(record);
+}
+
+function uploadData(){
+    //open a transaction on your db
+    const transaction = db.transaction(['new_budget'],'readwrite');
+
+    //access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    //get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        //if there was data in indexedDb's store, send to api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+              method: 'POST',
+              body: JSON.stringify(getAll.result),
+              headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+              }
+            })
+              .then(response => response.json())
+              .then(serverResponse => {
+                if (serverResponse.message) {
+                  throw new Error(serverResponse);
+                }
+                // open one more transaction
+                const transaction = db.transaction(['new_budget'], 'readwrite');
+                // access the new_budget object store
+                const budgetObjectStore = transaction.objectStore('new_budget');
+                // clear all items in your store
+                budgetObjectStore.clear();
+      
+                alert("All saved budget's has been submitted!");
+              })
+              .catch(err => {
+                console.log(err);
+              });
+          }
+        };
+    }
+
+//listen for app coming back online
+window.addEventListener('online',uploadData);
