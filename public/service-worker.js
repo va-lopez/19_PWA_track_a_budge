7e4a30516c3998722b29ab29a7e408773d00a474
valcomp <valerie.aleida.lopez@gmail.com>
2022-08-13 03:31:03 -0500

    added more files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,14 @@ const FILES_TO_CACHE = [
     "./css/styles.css",
     "./js/idb.js",
     "./js/index.js",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
     "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
   ];
 
 self.addEventListener('install', function (e) {
